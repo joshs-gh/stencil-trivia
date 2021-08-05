f5c7f1d291d1141ce7318ea01281b402d28cdbb8
jsiegel-cn <joshsaddress@gmail.com>
2021-08-05 12:00:25 -0400

    autolink

--- src/components/flash-card/flash-card.tsx
+++ src/components/flash-card/flash-card.tsx
@@ -1,4 +1,5 @@
 import { Component, h, Prop, State } from '@stencil/core';
+import Autolinker from 'autolinker';
 
 @Component({
   tag: 'flash-card',
@@ -13,13 +14,13 @@ export class FlashCard {
   private cardText: HTMLElement;
   private flip = () => {
     this.frontSide = !this.frontSide;
-    this.frontSide ? (this.cardText.innerHTML = this.question) : (this.cardText.innerHTML = this.answer);
+    this.frontSide ? (this.cardText.innerHTML = 'Q: ' + Autolinker.link(this.question)) : (this.cardText.innerHTML = 'A: ' + Autolinker.link(this.answer));
   };
 
   render() {
     return (
       <div class="card" onClick={this.flip}>
-        <span ref={e => (this.cardText = e as HTMLElement)}>{this.question}</span>
+        <span ref={e => (this.cardText = e as HTMLElement)}>{'Q: ' + Autolinker.link(this.question)}</span>
       </div>
     );
   }
--- src/components/trivia-loader/trivia-loader.tsx
+++ src/components/trivia-loader/trivia-loader.tsx
@@ -6,8 +6,22 @@ import { Component, h, Prop, State } from '@stencil/core';
   shadow: true,
 })
 export class TriviaLoader {
-  @Prop() gsheeturl: string;
+  @Prop() gsheeturl: string; // published GSheet w/ ?alt=json like https://spreadsheets.google.com/feeds/cells/1uu8QEql61g8XKA2_U-ELpkhQNGNaYvqSvA5tsbBK3QQ/1/public/values?alt=json
   @State() gsheet: any;
+
+  // https://bost.ocks.org/mike/shuffle/
+  private shuffle(array) {
+    var m = array.length,
+      t,
+      i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
 
   async componentWillLoad() {
     await fetch(this.gsheeturl)
@@ -22,6 +36,6 @@ export class TriviaLoader {
     for (let i = 2; i < this.gsheet.length; i += 2) {
       cards.push(<flash-card question={this.gsheet[i].content.$t} answer={this.gsheet[i + 1].content.$t}></flash-card>);
     }
-    return <div>{cards}</div>;
+    return <div>{this.shuffle(cards)}</div>;
   }
 }
